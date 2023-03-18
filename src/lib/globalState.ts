670c5a37641a26658e733baab9c1ec2d50ecb3b5
Treeskiii <mpio222@gmail.com>
2023-03-18 18:12:31 -0400

    first update

--- src/lib/globalState.ts
+++ src/lib/globalState.ts
@@ -4,13 +4,13 @@ import { Sync, Trigger } from 'ether-state';
 import { IMulticall } from '$lib/state/contracts';
 import { balanceOnBlock } from "$lib/stores/state";
 import { NETWORKS } from '$lib/config';
-import { networkProviders } from '$lib/stores/provider';
+import { networkProviders, walletAddress } from '$lib/stores/provider';
 
 // Mainnet Syncs
 new Sync([
   {
     trigger: Trigger.BLOCK,
-    input: () => ['0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'],
+    input: () => ['0x5d4444E6DC0e9E107b793b3DaAcD677551e0E1B1'],
     output: (value: [BigNumber]) => balanceOnBlock.set(value[0]),
     call: {
       target: () => NETWORKS[0].multicall2Address,
