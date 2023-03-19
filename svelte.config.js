6139ee2f05110d733fdb50b153bfa7c464594243
Treeskiii <mpio222@gmail.com>
2023-03-19 12:50:56 -0400

    updated adapter so this is one to revert before

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,7 @@
-import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess';
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: preprocess({ postcss: true }),
-
+import adapter from '@sveltejs/adapter-vercel';
+ 
+export default {
   kit: {
-    //trailingSlash: 'always',
-    adapter: adapter({
-      // default options are shown. On some platforms
-      // these options are set automatically — see below
-      pages: 'build',
-      assets: 'build',
-      fallback: 'index.html',
-      precompress: false
-    })
-  }
+    adapter: adapter(),
+  },
 };
-
-export default config;
-
-// updated
