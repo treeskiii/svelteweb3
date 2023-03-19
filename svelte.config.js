b8f5d4a2c56f24f266e53c599d19a35ae346bede
Treeskiii <mpio222@gmail.com>
2023-03-19 12:53:56 -0400

    try again

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,27 @@
+import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
+
 import adapter from '@sveltejs/adapter-vercel';
- 
-export default {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess({ postcss: true }),
+
   kit: {
-    adapter: adapter(),
-  },
+    //trailingSlash: 'always',
+    adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      runtime: 'nodejs18.x',
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html',
+      precompress: false
+    })
+  }
 };
+
+export default config;
+
+// updated
