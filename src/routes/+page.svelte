aa413b795664cb489649c46c05ab7c09ceb314be
mpio222 <mpio222@gmail.com>
2023-03-25 20:31:31 -0400

    updated

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 	} from '$lib/stores/provider';
 	import { balanceOnBlock } from '$lib/stores/state';
 	import { formatEther } from 'ethers/lib/utils';
-	import Logo from "$lib/public/piolabslogo.png";
+	import Logo from '$lib/public/piolabslogo.png';
 </script>
 
 <header>
@@ -97,48 +97,20 @@
 	</div>
 </header>
 
-<main class="flex flex-col p-6 w-screen h-screen items-center gap-5 font-serif  bg-gradient-to-r from-[#0d0d0d] to-black">
+<main
+	class="flex flex-col p-6 w-screen h-screen items-center gap-5 font-serif  bg-gradient-to-r from-[#0d0d0d] to-black"
+>
 	<h1 class="text-5xl text-white font-bold">Treeski's Web3 Starter Dapp</h1>
-	<div class="stats  glass ">
-		<div class="stat">
-		  <div class="stat-figure text-primary">
-			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
-		  </div>
-		  <div class="stat-title text-white">Connected Wallet</div>
-		  <div class="stat-value text-blue-300">{$walletAddress.substr(-4)}</div>
-		  <div class="stat-desc text-white">Connected: {$connected}</div>
-		</div>
-		
-		<div class="stat">
-		  <div class="stat-figure text-secondary">
-			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
-		  </div>
-		  <div class="stat-title text-white">Eth Balance</div>
-		  <div class="stat-value text-blue-300">{parseFloat(formatEther($balanceOnBlock)).toFixed(4)} ETH</div>
-		  <div class="stat-desc text-white">21% more than last month</div>
-		</div>
-		
-		<div class="stat">
-		  <div class="stat-figure text-blue-300">
-		  </div>
-		  <div class="stat-value text-white">86%</div>
-		  <div class="stat-title text-white">Tasks done</div>
-		  <div class="stat-desc text-blue-300">31 tasks remaining</div>
-		</div>
-		
-	  </div>
+
 	<div class="card w-96 glass mt-4">
 		<figure>
-			<img
-				src={Logo}
-				alt="piolabslogo"
-			/>
+			<img src={Logo} alt="piolabslogo" />
 		</figure>
 		<div class="card-body">
 			<p class="text-white">Wallet Address: {$walletAddress.substr(-4)}</p>
-			<h1  class="text-gray-300" >Eth Balance:</h1>
+			<h1 class="text-gray-300">Eth Balance:</h1>
 			<h2 class="card-title text-white ">{formatEther($balanceOnBlock)} ETH</h2>
-			
+
 			<div class="card-actions justify-end mt-4">
 				{#if !$connected}
 					<button class="btn btn-primary" on:click={connectMetamask}>Connect Wallet</button>
@@ -151,10 +123,7 @@
 	<div class="countdown" />
 
 	<div class="card" />
-	
 </main>
 
-
 <style>
-	
 </style>
